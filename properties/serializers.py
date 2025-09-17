efcc39458ad03a15e6eb95c3daf5681dadf2db51
kavyaFullstackDeveloper <kavya.why@gmail.com>
2025-09-17 13:50:26 +0530

    Allow CORS for https://urboven.vercel.app and frontend updates

--- properties/serializers.py
+++ properties/serializers.py
@@ -1,23 +1,30 @@
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 from django.contrib.auth.models import User
 from .models import Property, Favorite
 
 class UserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(
+        max_length=150,
+        validators=[UniqueValidator(User.objects.all(), message="Username already taken")]
+    )
+    password = serializers.CharField(write_only=True)
+
     class Meta:
         model = User
-        fields = ('id', 'username', 'password')
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ("id", "username", "password")
 
     def create(self, validated_data):
+        # ensure hashed password and username uniqueness handled by validator
         user = User.objects.create_user(**validated_data)
         return user
 
 class PropertySerializer(serializers.ModelSerializer):
     class Meta:
         model = Property
-        fields = '__all__'
+        fields = "__all__"
 
 class FavoriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Favorite
-        fields = ('id', 'user', 'property')
+        fields = ("id", "user", "property")
