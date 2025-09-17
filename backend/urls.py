2b0c7172dcaec4ef672fd740b6d819d8fe8f56dc
kavyaFullstackDeveloper <kavya.why@gmail.com>
2025-09-17 12:31:31 +0530

    Allow CORS for https://urboven.vercel.app and frontend updates

--- backend/urls.py
+++ backend/urls.py
@@ -2,8 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('properties.urls')),
+    path("", RedirectView.as_view(url="/api/", permanent=False)),
+    path("favicon.ico", RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
