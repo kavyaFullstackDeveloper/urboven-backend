170606e64502e1fe11319b22651d4a252858d897
kavyaFullstackDeveloper <kavya.why@gmail.com>
2025-09-17 17:41:37 +0530

    seeded

--- backend/settings.py
+++ backend/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,12 +69,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+"default": dj_database_url.config(
+default="postgresql://postgres:postgres@localhost:5432/urboven",
+conn_max_age=600,
+conn_health_checks=True,
+)
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
